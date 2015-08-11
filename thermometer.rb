f16629eb8f61c98b5deecc9378b43065d6e4b997
Hugh Kolias <hughkolias@gmail.com>
2015-08-10 22:03:28 -0400

    bug fix for directory

--- thermometer.rb
+++ thermometer.rb
@@ -2,7 +2,7 @@
 
 directory = '/sys/bus/w1/devices/'
 serial_number = '28-000005309f19'
-file_directory = directory + serial_number + 'w1_slave'
+file_directory = directory + serial_number + '/w1_slave'
 
 f = File.open(file_directory, 'r')
 2.times{ f.gets }
