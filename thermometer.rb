c2ce1af541cee1f5a0653f04fb2f5140eb594c69
Hugh Kolias <hughkolias@gmail.com>
2015-08-10 21:54:40 -0400

    test file

---
+++ thermometer.rb
@@ -0,0 +1,11 @@
+
+
+directory = '/sys/bus/w1/devices/'
+serial_number = '28-000005309f19'
+file_directory = directory + serial_number + 'w1_slave'
+
+f = File.open(file_directory, 'r')
+2.times{ f.gets }
+temp = $_.chomp.to_s
+puts "#{temp[29..-1].to_f/1000}#{"\xC2\xB0"}"C"
+
