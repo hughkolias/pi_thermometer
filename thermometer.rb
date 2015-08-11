a1ad7957cb50f660072d39cccbdf2b7cb21b856f
Hugh Kolias <hughkolias@gmail.com>
2015-08-10 22:11:43 -0400

    putting it into good ruby Class form

--- thermometer.rb
+++ thermometer.rb
@@ -1,11 +1,18 @@
 
+class Thermometer
 
-directory = '/sys/bus/w1/devices/'
-serial_number = '28-000005309f19'
-file_directory = directory + serial_number + '/w1_slave'
+	def initialize(SN = '28-000005309f19')
+		@serial_number = SN
+		@file_directory = '/sys/bus/w1/devices/' + @serial_number + '/w1_slave'
+	end
 
-f = File.open(file_directory, 'r')
-2.times{ f.gets }
-temp = $_.chomp.to_s
-puts "#{temp[29..-1].to_f/1000}#{"\xC2\xB0"}C"
+	def print_temperature
+		f = File.open(@file_directory, 'r')
+		2.times{ f.gets }
+		temp = $_.chomp.to_s
+		puts "#{temp[29..-1].to_f/1000}#{"\xC2\xB0"}C"
+	end
 
+end
+
+den = Thermometer.new.print_temperature
