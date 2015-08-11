218318220790e3f37956a01e4e7bc07a1814209e
Hugh Kolias <hughkolias@gmail.com>
2015-08-10 21:58:02 -0400

    bug fix

--- thermometer.rb
+++ thermometer.rb
@@ -7,5 +7,5 @@ file_directory = directory + serial_number + 'w1_slave'
 f = File.open(file_directory, 'r')
 2.times{ f.gets }
 temp = $_.chomp.to_s
-puts "#{temp[29..-1].to_f/1000}#{"\xC2\xB0"}"C"
+puts "#{temp[29..-1].to_f/1000}#{"\xC2\xB0"}C"
 
