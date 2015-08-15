1f56b680a26986faa7d837893184d97651141c5d
hugh kolias <hughkolias@gmail.com>
2015-08-15 22:29:43 +0000

    add relay control class

--- thermometer.rb
+++ thermometer.rb
@@ -1,3 +1,5 @@
+require 'pi_piper'
+include PiPiper
 
 class Thermometer
 
@@ -7,19 +9,72 @@ class Thermometer
 	end
 
 	def print_temperature
+		puts "The current temperature in the Den is #{get_temperature.to_s}#{"\xC2\xB0"}C"
+	end
+
+	def get_temperature
 		f = File.open(@file_directory, 'r')
 		2.times{ f.gets }
-		temp = $_.chomp.to_s
-		puts "#{temp[29..-1].to_f/1000}#{"\xC2\xB0"}C"
+		$_[29..-1].to_f/1000
+	end
+
+end
+
+class RelaySwitcher
+
+	attr_accessor :dir
+
+	def initialize
+		@heat = PiPiper::Pin.new(:pin => 17, :direction => :out)
+		@cool = PiPiper::Pin.new(:pin => 27, :direction => :out)
+		@fan = PiPiper::Pin.new(:pin =>22, :direction => :out)
+	end
+
+	def switch_on_heat
+		#@heat.output(:out)
+		@heat.on
+	end
+
+	def switch_off_heat
+		#@heat.output(:in)
+		@heat.off
+	end
+
+	def switch_on_cool
+		@cool.on
+	end
+
+	def switch_off_cool
+		@cool.off
+	end
+ 
+
+end
+
+class Logic
+
+	def initialize
+
 	end
 
 end
 
 den = Thermometer.new
+relay = RelaySwitcher.new
 
 i = 0
 
 while i < 1 do
 	den.print_temperature
-	sleep(5)
+	temperature = den.get_temperature.to_f
+	if temperature > 29.0
+		relay.switch_off_heat
+		sleep(2)
+		relay.switch_on_cool
+	else
+		relay.switch_off_cool
+		sleep(2)
+		relay.switch_on_heat
+	end
+	sleep(1)
 end
