9843b1675f3e094e592797515dcc7b0f2140dc42
Hugh Kolias <hughkolias@gmail.com>
2015-09-13 00:32:49 -0400

    bug fix

--- serverconn.rb
+++ serverconn.rb
@@ -3,16 +3,17 @@ require 'pg'
 class DbConnect
 
 	def self.read_webapp_user_set_temp
-		id = read_webapp_mode.cmd_tuples
-		return read_webapp_mode.getvalue(id-1,2) #must change 0 value to be last one
-		self.close
-
+		conn = pg_connect
+		id = conn.exec('SELECT * FROM thermostats').cmd_tuples
+		puts conn.exec('SELECT * FROM thermostats').getvalue(id-1,2) #must change 0 value to be last one
+		conn.close
 	end
 
 	def self.read_webapp_user_mode
-		id = read_webapp_mode.cmd_tuples
-		return read_webapp_mode.getvalue(id-1,1) #must change 0 value here
-		self.close
+		conn = pg_connect
+		id = conn.exec('SELECT * FROM thermostats').cmd_tuples
+		return conn.exec('SELECT * FROM thermostats').getvalue(id-1,1) #must change 0 value here
+		conn.close
 	end
 
 	def self.write_to_db(temp)
@@ -26,37 +27,33 @@ class DbConnect
 	private
 
 	def self.pg_connect
-		@connection = {:host => 'ec2-23-23-126-39.compute-1.amazonaws.com', 
-			  :dbname => 'dc383pgrh1blhu', :user => 'ihtyvhunepdkkl', 
-			  :port => 5432, 
-			  :password => 'T4CfPn_Vo1MTlB0IWuBqyBBfsm'}
+		@connection = {:host => 'ec2-23-23-126-39.compute-1.amazonaws.com', :dbname => 'dc383pgrh1blhu', :user => 'ihtyvhunepdkkl', :port => 5432, :password => 'T4CfPn_Vo1MTlB0IWuBqyBBfsm'}
 		PGconn.connect(@connection)
 	end
 
-	def self.read_temps_mode
-		pg_connect.exec('SELECT * FROM pitemps')
-	end 
+	#def self.read_temps_mode
+	#	pg_connect.exec('SELECT * FROM pitemps')
+	#end 
 
-	def self.read_webapp_mode
-		pg_connect.exec('SELECT * FROM thermostats')
-	end 
+	#def self.read_webapp_mode
+	#	pg_connect.exec('SELECT * FROM thermostats')
+	#end 
 
 	def self.write_temp_mode(temp)
-		id = read_temps_mode.cmd_tuples + 1
 		conn = pg_connect
+		id = conn.exec('SELECT * FROM pitemps').cmd_tuples + 1
 		conn.prepare('insert_values', 'insert into pitemps (id, temp, created_at, updated_at) values ($1, $2, $3, $4)')
 		conn.exec_prepared('insert_values', [ id, temp, Time.now, Time.now]) #these numbers should be variable
 		conn.close
 	end
 
 	def self.write_webapp_mode(temp, user_mode, user_set_temp)
-		id = read_webapp_mode.cmd_tuples + 1
 		conn = pg_connect
-		conn.prepare('insert_values', 'insert into thermostats (id, user_mode, user_set_temp, created_at, updated_at) values ($1, $2, $3, $4, $5, $6)')
+		id = conn.exec('SELECT * FROM thermostats').cmd_tuples + 1
+		conn.prepare('insert_values', 'insert into thermostats (id, user_mode, user_set_temp, created_at, updated_at) values ($1, $2, $3, $4, $5)')
 		conn.exec_prepared('insert_values', [ id, user_mode, user_set_temp, Time.now, Time.now]) #these numbers should be variable
 		conn.close
 	end
 
 end
 
-DbConnect.read_webapp_user_set_temp
