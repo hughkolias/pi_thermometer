6f7eab09e50a8273a31df0b6d4888a246c0e6dac
Hugh Kolias <hughkolias@gmail.com>
2015-09-13 01:04:24 -0400

    bug fix

--- serverconn.rb
+++ serverconn.rb
@@ -3,17 +3,19 @@ require 'pg'
 class DbConnect
 
 	def self.read_webapp_user_set_temp
-		conn = pg_connect
-		id = conn.exec('SELECT * FROM thermostats').cmd_tuples
-		conn.exec('SELECT * FROM thermostats').getvalue(id-1,2)
-		conn.close
+		@conn = pg_connect
+		id = @conn.exec('SELECT * FROM thermostats').cmd_tuples
+		@value = @conn.exec('SELECT * FROM thermostats').getvalue(id-1,2)
+		@conn.close
+		return @value
 	end
 
 	def self.read_webapp_user_mode
-		conn = pg_connect
-		id = conn.exec('SELECT * FROM thermostats').cmd_tuples
-		conn.exec('SELECT * FROM thermostats').getvalue(id-1,1)
-		conn.close
+		@conn = pg_connect
+		id = @conn.exec('SELECT * FROM thermostats').cmd_tuples
+		@value = @conn.exec('SELECT * FROM thermostats').getvalue(id-1,1)
+		@conn.close
+		return @value
 	end
 
 	def self.write_to_db(temp)
@@ -40,19 +42,19 @@ class DbConnect
 	#end 
 
 	def self.write_temp_mode(temp)
-		conn = pg_connect
-		id = conn.exec('SELECT * FROM pitemps').cmd_tuples + 1
-		conn.prepare('insert_values', 'insert into pitemps (id, temp, created_at, updated_at) values ($1, $2, $3, $4)')
-		conn.exec_prepared('insert_values', [ id, temp, Time.now, Time.now]) #these numbers should be variable
-		conn.close
+		@conn = pg_connect
+		id = @conn.exec('SELECT * FROM pitemps').cmd_tuples + 1
+		@conn.prepare('insert_values', 'insert into pitemps (id, temp, created_at, updated_at) values ($1, $2, $3, $4)')
+		@conn.exec_prepared('insert_values', [ id, temp, Time.now, Time.now]) #these numbers should be variable
+		@conn.close
 	end
 
 	def self.write_webapp_mode(temp, user_mode, user_set_temp)
-		conn = pg_connect
-		id = conn.exec('SELECT * FROM thermostats').cmd_tuples + 1
-		conn.prepare('insert_values', 'insert into thermostats (id, user_mode, user_set_temp, created_at, updated_at) values ($1, $2, $3, $4, $5)')
-		conn.exec_prepared('insert_values', [ id, user_mode, user_set_temp, Time.now, Time.now]) #these numbers should be variable
-		conn.close
+		@conn = pg_connect
+		id = @conn.exec('SELECT * FROM thermostats').cmd_tuples + 1
+		@conn.prepare('insert_values', 'insert into thermostats (id, user_mode, user_set_temp, created_at, updated_at) values ($1, $2, $3, $4, $5)')
+		@conn.exec_prepared('insert_values', [ id, user_mode, user_set_temp, Time.now, Time.now]) #these numbers should be variable
+		@conn.close
 	end
 
 end
